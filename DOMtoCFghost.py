45d95326f55074c121415439b41ff943d348c2a4
jakao <cykao0907@gmail.com>
2024-06-07 22:53:05 +0800

    update dom to ghost

--- DOMtoCFghost.py
+++ DOMtoCFghost.py
@@ -16,12 +16,13 @@ CONTEST_ID = 1
 ADMIN_USERNAME = ''
 ADMIN_PASSWORD = ''
 
-contest = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}').json()
-teams = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}/teams').json()
-valid_team_ids = set(team['id'] for team in teams)
-problems = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}/problems').json()
-problems_dict = {problem['id']: problem for problem in problems}
 auth = requests.auth.HTTPBasicAuth(ADMIN_USERNAME, ADMIN_PASSWORD)
+
+contest = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}', auth=auth).json()
+teams = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}/teams', auth=auth).json()
+valid_team_ids = set((team['id'] if team['group_ids'][0] != '1' else '') for team in teams)
+problems = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}/problems', auth=auth).json()
+problems_dict = {problem['id']: problem for problem in problems}
 submissions = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}/submissions', auth=auth).json()
 judgements = requests.get(f'{BASE_URL}/api/contests/{CONTEST_ID}/judgements', auth=auth).json()
 judgements_dict = {judgement['submission_id']: judgement for judgement in judgements}
@@ -39,8 +40,12 @@ print('@submissions', len(submissions))
 for problem in problems:
     print('@p', ','.join([problem['label'], problem['name'], '20', '0']))
 
+teamID = dict()
+
 for team in teams:
-    print('@t', ','.join([team['id'], '0', '1', f"\"{team['name']}\""]))
+    if team['id'] not in teamID:
+        teamID[team['id']] = str(len(teamID)+1)
+    print('@t', ','.join([teamID[team['id']], '0', '1', f"\"{team['name']}\""]))
     # print('@t', ','.join([team['id'], '0', '1', f"\"{team['name']} ({team['affiliation']})\""]))
 
 result_map = {
@@ -59,8 +64,10 @@ for submission in submissions:
     if team_id in valid_team_ids:
         problem_id = problems_dict[submission['problem_id']]['label']
         submission_counter[(team_id, problem_id)] += 1
-        print('@s', ','.join([team_id,
-                            problem_id,
-                            str(submission_counter[(team_id, problem_id)]),
-                            str(int(to_timedelta(submission['contest_time']).total_seconds())),
-                            result_map[judgements_dict[submission['id']]['judgement_type_id']]]))
+        if submission['id'] in judgements_dict:
+            # print(judgements_dict[submission['id']])
+            print('@s', ','.join([teamID[team_id],
+                                problem_id,
+                                str(submission_counter[(team_id, problem_id)]),
+                                str(int(to_timedelta(submission['contest_time']).total_seconds())),
+                                result_map[judgements_dict[submission['id']]['judgement_type_id']]]))
